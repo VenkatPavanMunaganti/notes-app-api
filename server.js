5e72e1866229b1475cfa50e74173a1f27ecde6ba
VenkatPavanMunaganti <109862602+VenkatPavanMunaganti@users.noreply.github.com>
2023-03-18 13:06:29 -0400

    Updated server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors= require("cors")
+const cors = require("cors")
 
 const app = express();
 app.use(cors())
@@ -37,19 +37,23 @@ let notes = [
 //To DO: Write the end point for getting list of notes
 // GET /api/notes
 app.get("/api/notes", (req, res) => {
-  
+
 });
 
 //To DO: Write the end point to add new note to the list
 // POST /api/notes
 app.post("/api/notes", (req, res) => {
-  
+
 });
 
 //To DO: Write an endpoint to delete from the list
 // DELETE /api/notes/:id
 app.delete("/api/notes/:id", (req, res) => {
-  
+
+  //Disable cache
+  res.set("Cache-Control", "no-cache");
+  res.set("Pragma", "no-cache");
+
 });
 
 //To Do: write the code to start the server in port 5000
